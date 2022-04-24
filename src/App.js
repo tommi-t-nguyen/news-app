76e931221e43667a08b2fe646e3a6bfd0afc3f81
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2022-04-24 16:17:23 -0400

    Create basic page layout and route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './Pages/Home/Home.js';
+import Error from './Pages/Error/Error.js';
+import Detail from './Pages/Detail/Detail.js';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className='App'>
+    <Switch>
+      <Route
+        exact path='/'
+        render={() => <Home/>}
+      />
+      <Route
+        exact path='/article/:id'
+        render={({match}) =>
+        <Detail id={match.params.id}/> }
+      />
+      <Route path='/error' component={Error}/>
+      <Redirect to='/error'/>
+    </Switch>
+  </main>
+  )
 }
 
-export default App;
+export default App
