044fd11160817b9d30b45ccd7d552afd002bb0f7
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2022-04-25 17:34:40 -0400

    Add error page style

--- src/Pages/Error/Error.js
+++ src/Pages/Error/Error.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Error.css'
 
 const Error = () => {
   return (
-    <div>404 PAGE NOT FOUND</div>
+    <div className="error">
+      <h2>404 PAGE NOT FOUND</h2>
+    <Link to={'/'}>Back Home</Link>
+    </div>
   )
 }
 
